1179ac3906ff5848e20516bf4bdcc5262787343f
cau <cau@salle222-12.arda>
2018-01-12 16:57:49 +0100

    Matrice en plus

--- funstionsm.js
+++ funstionsm.js
@@ -15,6 +15,45 @@ function getPeopleVoteNumber(matiere, name)
 		else return ( 1 / x );
 	}
 
+	function createD(matiere, matrix)
+	{
+		
+		var em = emptyMatrix();
+		int x = 0;
+
+		for ( votant in em)
+		{
+			for ( cible in votant )
+			{
+				for ( vote in votes[votant])
+				{
+					if ( cible == vote )
+					{
+						em[votant][cible] = matrix[votant][matiere];
+					}
+				}
+			}
+		}
+
+		return em;
+	}
+
+	function emptyMatrix()
+	{
+		var table = new Array();
+		var x = 0;
+		for ( people in votes)
+		{
+			table[people] = new Array();
+			for ( people2 in votes )
+			{
+				table[people][people2] = 0;
+			}
+		}
+
+		return table;
+	}
+
 	function createTableAllPeopleWeight()
 	{
 		var table = new Array();
