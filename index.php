e2ccd8e62b9a5eb7e217ff551c4575ee8f4482de
N0fix <neolipop2@hotmail.fr>
2018-01-17 16:03:48 +0100

    Optimisation & commentary

--- index.php
+++ index.php
@@ -6,33 +6,74 @@
   <meta charset="utf-8" />
   <script src="http://www.iut-fbleau.fr/projet/maths/?rah_external_output=pagerank.js"></script>
   <script src="http://www.iut-fbleau.fr/projet/maths/?rah_external_output=logins.js"></script>
-
-
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+  <script src="js/funstionsm.js"></script>
 </head>
-<h3>Please refresh the page between each rank calcul</h3>
-<body>
+<center>
+  <h3>Please refresh the page between each rank calcul</h3>
+</center>
+<body style="margin:5%">
+  <h3>Matière</h3>
   <form action="index.php" method="post">
-    Matière:<br>
-      ACDA : <input type="checkbox" class="matiere[]" value="ACDA"><br>
-      ANG : <input type="checkbox" class="matiere[]" value="ANG"><br>
-      APL : <input type="checkbox" class="matiere[]" value="APL"><br>
-      ART : <input type="checkbox" class="matiere[]" value="ART"><br>
-      ASR : <input type="checkbox" class="matiere[]" value="ASR"><br>
-      EC : <input type="checkbox" class="matiere[]" value="EC"><br>
-      EGOD : <input type="checkbox" class="matiere[]" value="EGOD"><br>
-      MAT : <input type="checkbox" class="matiere[]" value="MAT"><br>
-      SGBD : <input type="checkbox" class="matiere[]" value="SGBD"><br>
-      SPORT : <input type="checkbox" class="matiere[]" value="SPORT"><br>
+
+    <p>
+      <input type="checkbox" class="matiere[]" id="ACDA" value="ACDA"/>
+      <label for="ACDA">ACDA</label>
+    </p>
+
+    <p>
+      <input type="checkbox" class="matiere[]" id="ANG" value="ANG"/>
+      <label for="ANG">ANG</label>
+    </p>
+    <p>
+      <input type="checkbox" class="matiere[]" id="APL" value="APL"/>
+      <label for="APL">APL</label>
+    </p>
+    <p>
+      <input type="checkbox" class="matiere[]" id="ART" value="ART"/>
+      <label for="ART">ART</label>
+    </p>
+    <p>
+      <input type="checkbox" class="matiere[]" id="ASR" value="ASR"/>
+      <label for="ASR">ASR</label>
+    </p>
+    <p>
+      <input type="checkbox" class="matiere[]" id="EC" value="EC"/>
+      <label for="EC">EC</label>
+    </p>
+    <p>
+      <input type="checkbox" class="matiere[]" id="EGOD" value="EGOD"/>
+      <label for="EGOD">EGOD</label>
+    </p>
+    <p>
+      <input type="checkbox" class="matiere[]" id="MAT" value="MAT"/>
+      <label for="MAT">MAT</label>
+    </p>
+    <p>
+      <input type="checkbox" class="matiere[]" id="SGBD" value="SGBD"/>
+      <label for="SGBD">SGBD</label>
+    </p>
+    <p>
+      <input type="checkbox" class="matiere[]" id="SPORT" value="SPORT"/>
+      <label for="SPORT">SPORT</label>
+    </p>
+
+
   </form>
   <br />
-  <button onclick="printScores()">Afficher le classement</button>
+  <button class="waves-effect waves-light btn" onclick="printScores()">Afficher le classement</button>
 
-  <table id="table">
-    <th>
+  <table id="table" class="highlight">
+    <thead>
       <tr>
-
+        <th>
+          Rang
+        </th>
+        <th>
+          Nom
+        </th>
       </tr>
-    </th>
+    </thead>
     <td>
 
     </td>
@@ -42,379 +83,3 @@
 
 </body>
 </html>
-
-
-<script>
-console.log(getNumberParticipants());
-function printScores(){
-
-
-  var matiere = getCheckedMatiere();
-
-  var dict = newDictionnary();
-
-  var resultMatrix = emptyMatrix();
-  console.log("Matiere length " + matiere.length);
-
-  for(var i = 0; i < matiere.length; i++){
-    var matrix = createD(matiere[i]);
-    matrix = multiplyMatrixWithValue(matrix, 0.90);
-    var secondMatrix = emptyMatrix(); //Looks like it works fine
-    secondMatrix = addMatrixWithValue(secondMatrix, getDefaultVoteValue()); //Non empty matrix
-    secondMatrix = multiplyMatrixWithValue(secondMatrix, 0.10); //Let's just multiply this with a value
-    let endMatrix = addMatrix(matrix, secondMatrix);
-    console.log("Speedtest 1");
-    let array = matrixToArray(endMatrix);
-    for(let j = 0; j < 3; j++){
-      array = multiplyTwoArray(array, array);
-      //endMatrix = multiplyTwoMatrix(endMatrix, endMatrix);
-    }
-
-    console.log("Speedtest 2");
-    let tmpMatrix = arrayToMatrix(array);
-    dict = dictAndMatrix(dict, tmpMatrix);
-  }
-  dict = sortDictionnary(dict);
-  generateTab(dict);
-  console.log("Speedtest 3");
-}
-
-function generateTab(dict){
-  var table = document.getElementById("table").getElementsByTagName("tbody")[0];
-  let nb = getNumberParticipants();
-  let w = 0;
-  for(login in dict){
-    let newRow = table.insertRow(w);
-    let cellrang = newRow.insertCell(0);
-    let cellnom = newRow.insertCell(1);
-    cellrang.innerHTML = ""+(w+1);
-    cellnom.innerHTML  = ""+loginToName(dict[login][0]);
-    w++;
-  }
-}
-/* MATRIX MANIPULATION
-*
-*
-*
-MATRIX MANIPULATION */
-
-/* Set matrix values into the dict*/
-function dictAndMatrix(dict, matrix){
-  for(name in matrix){
-    var val = 0;
-    for(votedname in matrix){
-      dict[votedname] += matrix[name][votedname];
-    }
-  }
-  return dict;
-}
-function loginToName(login){
-  for(name in logins){
-    for(nique in name){
-      if(login == name){
-        return logins[name];
-      }
-    }
-  }
-  return "error";
-}
-function multiplyTwoMatrix(matrix1, matrix2){
-  let resultMatrix = [];
-  let sum = 0;
-  for(i in matrix1){
-    resultMatrix[i] = [];
-    for(j in matrix2){
-      sum = 0;
-      for(k in matrix1){
-        sum += matrix1[i][k] * matrix2[k][j];
-      }
-      resultMatrix[i][j] = sum;
-    }
-  }
-  return resultMatrix;
-}
-function multiplyTwoArray(array1, array2){
-
-  var result = new Array();
-    for (var i = 0; i < array1.length; i++) {
-        result[i] = new Array();
-        for (var j = 0; j < array2[0].length; j++) {
-            var sum = 0;
-            for (var k = 0; k < array1[0].length; k++) {
-                sum += array1[i][k] * array2[k][j];
-            }
-            result[i][j] = sum;
-        }
-    }
-    return result;
-}
-
-
-/*returns 0,1,2,3... index array of the given matrix*/
-function matrixToArray(matrix){
-  let resultArray = new Array();
-  let whoVoted = 0;
-  for(name in matrix){
-    resultArray[whoVoted] = new Array();
-    let whoVotedForWho = 0;
-    for(votedname in matrix){
-      resultArray[whoVoted][whoVotedForWho] = matrix[name][votedname];
-      if(whoVoted == 0 && whoVotedForWho == 12) console.log(matrix[name][votedname]);
-      whoVotedForWho++;
-    }
-    whoVoted++;
-  }
-  console.log(resultArray[0][12]);
-  return resultArray;
-}
-
-function arrayToMatrix(array){
-  let matrix = [];
-  let indextonamei = "";
-  for(let i = 0 ; i < array.length; i++){
-    indextonamei = indexToName(i);
-    matrix[indextonamei] = [];
-    for(let j = 0; j < array[i].length; j++){
-      matrix[indextonamei][indexToName(j)] = array[i][j];
-    }
-  }
-  return matrix;
-}
-
-function multiplyMatrixWithValue(matrix, value){
-  for(name in matrix){
-    for(votedname in matrix[name]){
-        matrix[name][votedname] *= value;
-    }
-  }
-  return matrix;
-}
-function addMatrix(matrix1, matrix2){
-  var matrix = emptyMatrix();
-  for(name in matrix1){
-    for(votedname in matrix1){
-      matrix[name][votedname] = matrix1[name][votedname] + matrix2[name][votedname]
-    }
-  }
-  return matrix;
-}
-function addMatrixWithValue(matrix, value){
-  for(name in matrix){
-    for(votedname in matrix[name]){
-        matrix[name][votedname] += value;
-    }
-  }
-  return matrix;
-}
-function getCheckedMatiere(){
-  var matiere = [];
-  var array = document.getElementsByClassName("matiere[]");
-  for(var i = 0; i < array.length; i++){
-    if(array[i].checked){
-      matiere.push(array[i].value);
-    }
-  }
-  return matiere;
-}
-/* DICTIONNARY HANDLING
-*
-*
-*
-DICTIONNARY HANDLING */
-
-function indexToName(index){
-  let i = 0;
-  for(name in logins){
-    if(i == index) return name;
-    i++;
-  }
-  return "error";
-}
-
-function sortDictionnary(dict){
-  var items = Object.keys(dict).map(function(key) {
-      return [key, dict[key]];
-  });
-  // Sort the array based on the second element
-  items.sort(function(first, second) {
-      return second[1] - first[1];
-  });
-  // Create a new array with only the first 5 items
-  return items;
-}
-
-
-function newDictionnary(){
-
-  var dictionnary = [];
-  let count = 0;
-  for(name in logins){
-    dictionnary[name] = 0;
-    count++;
-  }
-  console.log("Number of members = "+count);
-  return dictionnary;
-}
-
-
-  /* TESTS
-  *
-  *
-  *
-    TESTS */
-
-  function showAllLogins(){
-    var text = "";
-    for(x in logins){
-      console.log(x + " = " + logins[x] + "");
-    }
-  }
-
-
-
-
-  function showAllVotes(){
-    var text = "";
-    for(name in votes){
-      for(matiere in votes[name]){
-        for(votedname in votes[name][matiere]){
-          text += name + " voted in " + matiere + " : "  + votes[name][matiere][votedname] + "<br />";
-          document.getElementById("test").innerHTML = text;
-        }
-      }
-    }
-  }
-
-
-
-
-
-
-  /* OTHER USEFULL STUFF
-  *
-  *
-  *
-    OTHER USEFULL STUFF -Guillaume-*/
-
-    function getNumberParticipants(){
-      let value = 0;
-      for(name in logins){
-        value++;
-      }
-      return value;
-    }
-    function getDefaultVoteValue(){
-      var value = 0;
-      for(name in logins){
-        value++;
-      }
-      return 1/value;
-    }
-
-
-
-  function getPeopleVoteNumber(matiere, name)
-  	{
-  		var value = 0;
-  		for(x in votes[name][matiere])
-  		{
-  			value += 1;
-  		}
-  		return value;
-
-  	}
-  	function getPeopleVoteValue(matiere, name)
-  	{
-  		var x = getPeopleVoteNumber(matiere,name);
-  		if ( x == 0) return 0;
-  		else return ( 1 / x );
-  	}
-
-  	function createTableAllPeopleWeight()
-  	{
-  		var table = new Array();
-  		var x;
-  		for ( name in votes )
-  		{
-  			table[name] = new Array();
-  			for ( matiere in votes[name] )
-  			{
-  				table[name][matiere] = getPeopleVoteValue(matiere, name);
-  			}
-  		}
-
-  		return table;
-  	}
-
-  	function votantExiste(votant) /* A coder , il n'y a pas le votant dans l'obj si il a pas voté... */
-  	{
-  		if ( votes[votant] == null)
-  			return false
-  		return true;
-  	}
-
-  	function createD(matiere)
-  	{
-
-  		var em = emptyMatrix();
-
-  		var matrix = createTableAllPeopleWeight();
-
-  		var x = 0;
-
-  		for ( votant in em)
-  		{
-  			for ( cible in em[votant] )
-  			{
-
-  				if ( votantExiste(votant) )
-  				{
-
-	  				for ( vote in votes[votant][matiere] )
-	  				{
-	  					if ( cible == votes[votant][matiere][vote] )
-	  					{
-	  						em[votant][cible] = matrix[votant][matiere];
-	  					}
-
-	  				}
-	  			}
-  			}
-  		}
-
-  		for( i in em)
-  		{
-  			for ( j in em[i] )
-  			{
-  				x += em[i][j];
-  			}
-
-  			if ( x == 0 )
-  			{
-  				for ( j in em[i]  )
-  				{
-  					em[i][j] = getDefaultVoteValue();
-  				}
-  			}
-  			x = 0
-  		}
-
-  		return em;
-  	}
-
-  	function emptyMatrix()
-  	{
-  		var table = new Array();
-  		var g = getNumberParticipants();
-  		for ( x in logins )
-  		{
-  			table[x] = new Array();
-  			for ( y in logins  )
-  			{
-  				table[x][y] = 0;
-  			}
-  		}
-
-  		return table;
-}
-</script>
---
+++ js/funstionsm.js
@@ -0,0 +1,376 @@
+
+
+
+/*This function prints calculate and print the score */
+function printScores(){
+  var matiere = getCheckedMatiere();
+  var dict = newDictionnary();
+  var resultMatrix = emptyMatrix();
+  console.log("Matiere length " + matiere.length);
+  if(matiere.length == 0) return;
+  for(var i = 0; i < matiere.length; i++){
+    var matrix = createD(matiere[i]);
+    matrix = multiplyMatrixWithValue(matrix, 0.90);
+    var secondMatrix = emptyMatrix(); //Looks like it works fine
+    secondMatrix = addMatrixWithValue(secondMatrix, getDefaultVoteValue()); //Non empty matrix
+    secondMatrix = multiplyMatrixWithValue(secondMatrix, 0.10); //Let's just multiply this with a value
+    let endMatrix = addMatrix(matrix, secondMatrix);
+    console.log("Speedtest 1");
+    let array = matrixToArray(endMatrix);
+    for(let j = 0; j < 3; j++){
+      array = multiplyTwoArray(array, array);
+      //endMatrix = multiplyTwoMatrix(endMatrix, endMatrix);
+    }
+    console.log("Speedtest 2");
+    let tmpMatrix = arrayToMatrix(array);
+    dict = dictAndMatrix(dict, tmpMatrix);
+  }
+  dict = sortDictionnary(dict);
+  generateTab(dict);
+  console.log("Speedtest 3");
+}
+
+/* Generate HTML table depending on the dict(name:value)*/
+function generateTab(dict){
+  var table = document.getElementById("table").getElementsByTagName("tbody")[0];
+  let nb = getNumberParticipants();
+  let w = 0;
+  for(login in dict){
+    let newRow = table.insertRow(w);
+    let cellrang = newRow.insertCell(0);
+    let cellnom = newRow.insertCell(1);
+    cellrang.innerHTML = ""+(w+1);
+    cellnom.innerHTML  = ""+loginToName(dict[login][0]);
+    w++;
+  }
+}
+
+/* MATRIX MANIPULATION
+*
+*
+*
+MATRIX MANIPULATION */
+
+/* Set matrix values into the dict(name:value)
+Ex :
+MATRIX =
+      aslan beta
+aslan   0.5   1
+beta    0.5   0
+
+The function will return the dict :
+aslan : 1.5
+beta : 0.5
+*/
+function dictAndMatrix(dict, matrix){
+  for(name in matrix){
+    var val = 0;
+    for(votedname in matrix){
+      dict[votedname] += matrix[name][votedname];
+    }
+  }
+  return dict;
+}
+
+/* This function return the full name of an user depending on it's login*/
+function loginToName(login){
+  for(name in logins){
+    for(nique in name){
+      if(login == name){
+        return logins[name];
+      }
+    }
+  }
+  return "error";
+}
+
+/*This function multiplies two matrix and return a matrix as a result*/
+function multiplyTwoMatrix(matrix1, matrix2){
+  let resultMatrix = [];
+  let sum = 0;
+  for(i in matrix1){
+    resultMatrix[i] = [];
+    for(j in matrix2){
+      sum = 0;
+      for(k in matrix1){
+        sum += matrix1[i][k] * matrix2[k][j];
+      }
+      resultMatrix[i][j] = sum;
+    }
+  }
+  return resultMatrix;
+}
+
+/*This function multiplies two arrays and return a arrays as a result*/
+
+function multiplyTwoArray(array1, array2){
+
+  var result = new Array();
+    for (var i = 0; i < array1.length; i++) {
+        result[i] = new Array();
+        for (var j = 0; j < array2[0].length; j++) {
+            var sum = 0;
+            for (var k = 0; k < array1[0].length; k++) {
+                sum += array1[i][k] * array2[k][j];
+            }
+            result[i][j] = sum;
+        }
+    }
+    return result;
+}
+
+
+/*returns 0,1,2,3... indexed array of the given name indexed matrix*/
+function matrixToArray(matrix){
+  let resultArray = new Array();
+  let whoVoted = 0;
+  for(name in matrix){
+    resultArray[whoVoted] = new Array();
+    let whoVotedForWho = 0;
+    for(votedname in matrix){
+      resultArray[whoVoted][whoVotedForWho] = matrix[name][votedname];
+      whoVotedForWho++;
+    }
+    whoVoted++;
+  }
+
+  return resultArray;
+}
+
+/*Returns a name indexed matrix depending on an 0.1.2 indexed array*/
+function arrayToMatrix(array){
+  let matrix = [];
+  let indextonamei = "";
+  for(let i = 0 ; i < array.length; i++){
+    indextonamei = indexToName(i);
+    matrix[indextonamei] = [];
+    for(let j = 0; j < array[i].length; j++){
+      matrix[indextonamei][indexToName(j)] = array[i][j];
+    }
+  }
+  return matrix;
+}
+
+
+/*Multiply a matrix with a value*/
+
+function multiplyMatrixWithValue(matrix, value){
+  for(name in matrix){
+    for(votedname in matrix[name]){
+        matrix[name][votedname] *= value;
+    }
+  }
+  return matrix;
+}
+
+/*Sums two matrixs*/
+
+function addMatrix(matrix1, matrix2){
+  var matrix = emptyMatrix();
+  for(name in matrix1){
+    for(votedname in matrix1){
+      matrix[name][votedname] = matrix1[name][votedname] + matrix2[name][votedname]
+    }
+  }
+  return matrix;
+}
+
+
+/*Sums a matrix with a value*/
+function addMatrixWithValue(matrix, value){
+  for(name in matrix){
+    for(votedname in matrix[name]){
+        matrix[name][votedname] += value;
+    }
+  }
+  return matrix;
+}
+
+/*Get all checked checkboxes on the html form*/
+function getCheckedMatiere(){
+  var matiere = [];
+  var array = document.getElementsByClassName("matiere[]");
+  for(var i = 0; i < array.length; i++){
+    if(array[i].checked){
+      matiere.push(array[i].value);
+    }
+  }
+  return matiere;
+}
+/* DICTIONNARY HANDLING
+*
+*
+*
+DICTIONNARY HANDLING */
+
+/*Allows to know the name of the user based on an index based on "logins" variable*/
+function indexToName(index){
+  let i = 0;
+  for(name in logins){
+    if(i == index) return name;
+    i++;
+  }
+  return "error";
+}
+
+
+/*Sorting dictionnary(name:value) by value*/
+function sortDictionnary(dict){
+  var items = Object.keys(dict).map(function(key) {
+      return [key, dict[key]];
+  });
+  // Sort the array based on the second element
+  items.sort(function(first, second) {
+      return second[1] - first[1];
+  });
+  // Create a new array with only the first 5 items
+  return items;
+}
+
+
+/*Create a new dictionnary(name:login) using all names on "logins" variable as keys, and
+setting all values to 0*/
+function newDictionnary(){
+
+  var dictionnary = [];
+  let count = 0;
+  for(name in logins){
+    dictionnary[name] = 0;
+    count++;
+  }
+  console.log("Number of members = "+count);
+  return dictionnary;
+}
+
+
+  /* OTHER USEFULL STUFF
+  *
+  *
+  *
+    OTHER USEFULL STUFF*/
+
+
+/*Returns number of participants based on "logins" variable*/
+  function getNumberParticipants(){
+    let value = 0;
+    for(name in logins){
+      value++;
+    }
+    return value;
+  }
+
+/*Returns default vote value for someone that didn't vote*/
+  function getDefaultVoteValue(){
+    var value = 0;
+    for(name in logins){
+      value++;
+    }
+    return 1/value;
+  }
+
+
+/*Know the number of people someone voted on a matiere*/
+  function getPeopleVoteNumber(matiere, name)
+  	{
+  		var value = 0;
+  		for(x in votes[name][matiere])
+  		{
+  			value += 1;
+  		}
+  		return value;
+
+  	}
+  	function getPeopleVoteValue(matiere, name)
+  	{
+  		var x = getPeopleVoteNumber(matiere,name);
+  		if ( x == 0) return 0;
+  		else return ( 1 / x );
+  	}
+
+  	function createTableAllPeopleWeight()
+  	{
+  		var table = new Array();
+  		var x;
+  		for ( name in votes )
+  		{
+  			table[name] = new Array();
+  			for ( matiere in votes[name] )
+  			{
+  				table[name][matiere] = getPeopleVoteValue(matiere, name);
+  			}
+  		}
+
+  		return table;
+  	}
+
+  	function votantExiste(votant) /* A coder , il n'y a pas le votant dans l'obj si il a pas voté... */
+  	{
+  		if ( votes[votant] == null)
+  			return false
+  		return true;
+  	}
+
+  	function createD(matiere)
+  	{
+
+  		var em = emptyMatrix();
+
+  		var matrix = createTableAllPeopleWeight();
+
+  		var x = 0;
+
+  		for ( votant in em)
+  		{
+  			for ( cible in em[votant] )
+  			{
+
+  				if ( votantExiste(votant) )
+  				{
+
+	  				for ( vote in votes[votant][matiere] )
+	  				{
+	  					if ( cible == votes[votant][matiere][vote] )
+	  					{
+	  						em[votant][cible] = matrix[votant][matiere];
+	  					}
+
+	  				}
+	  			}
+  			}
+  		}
+
+  		for( i in em)
+  		{
+  			for ( j in em[i] )
+  			{
+  				x += em[i][j];
+  			}
+
+  			if ( x == 0 )
+  			{
+  				for ( j in em[i]  )
+  				{
+  					em[i][j] = getDefaultVoteValue();
+  				}
+  			}
+  			x = 0
+  		}
+
+  		return em;
+  	}
+
+  	function emptyMatrix()
+  	{
+  		var table = new Array();
+  		var g = getNumberParticipants();
+  		for ( x in logins )
+  		{
+  			table[x] = new Array();
+  			for ( y in logins  )
+  			{
+  				table[x][y] = 0;
+  			}
+  		}
+
+  		return table;
+}
